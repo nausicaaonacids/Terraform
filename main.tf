a2e9252b0bbd1a77b4d90654c5efe7a54ac4685e
nausicaaonacids <64554555+nausicaaonacids@users.noreply.github.com>
2020-07-23 20:36:05 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "docker" {
+  version = "~> 2.7"
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
